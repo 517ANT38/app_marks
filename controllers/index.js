a326dafcfe12c433327438d47c41f6c9ff665ac7
517ANT38 <102868848+517ANT38@users.noreply.github.com>
2023-06-29 13:33:58 +0400

    CREATE_INDEX_FILE_CONTROLLERS

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const answer=require("./answerController");
+const question=require("./questionController");
+const user=require("./userController");
+const objectSight=require("./objectSightController");
+
+module.exports.controllers={
+    answer,
+    question,
+    user,
+    objectSight
+};
