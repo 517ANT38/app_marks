c31e4e660a24454c346bdc44f03bb2ed6aedb117
517ANT38 <102868848+517ANT38@users.noreply.github.com>
2023-07-09 20:36:41 +0400

    WRITEBASE64_In_file

--- util/util.js
+++ util/util.js
@@ -2,19 +2,21 @@ const { randomUUID } = require("crypto");
 const fs=require("fs/promises");
 const path = require("path");
 const { UUID } = require("sequelize");
-const base64ToBuffer = s=>Buffer.from(s,'base64');
+const reg=/data:image\/[~_,\w]{2,}\;/g;
+const base64ToBuffer = s=>Buffer.from(s.replace(reg+/base64,/,""),'base64');
 
 const bufferToBase64=theBuff=>theBuff.toString('base64');
 
 
 const createNameFile=(img)=>{
-    const reg=/data:image\/[~_,\w]{2,}\;/g;
+   
     const arr=reg.exec(img);  
     
     let uuid=randomUUID();
     if(!arr)return uuid +".bin";
     const type=arr[0].split("/")
-    return uuid+"."+type;
+    
+    return uuid+"."+type[1].replace(";","");
 }
 const asyncHandler=func => async (req, res, next) => {
     try {
