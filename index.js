fabb189f627cd59e8f60c11bbe9d7f8c09e8bdb0
517ANT38 <102868848+517ANT38@users.noreply.github.com>
2023-07-16 12:38:08 +0400

    set_cors

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-
 const express = require("express");
-
-
-
 const bodyParser=require("body-parser");
 const models=require("./models");
 const controllers=require("./controllers");
@@ -12,7 +8,13 @@ const services=require("./services");
 const PORT = process.env.PORT || 8080;
 const router=express.Router;
 const app=express();
-
+app.use((req,res,next)=>{
+   res.setHeader("Access-Control-Allow-Origin","*");
+   res.setHeader("Access-Control-Allow-Methods","*");
+   res.setHeader("Access-Control-Allow-Headers","origin,content-type,accept");
+   res.setHeader("Content-Security-Policy"," default-src *; connect-src *; script-src *; object-src *;")
+   next();
+});
 app.use("/images",express.static(__dirname+"/static"));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
