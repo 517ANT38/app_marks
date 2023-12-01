d8713edbcbc5e185695eef4de3868c309daeff81
517ANT38 <a.tashchilin@bk.com>
2023-12-01 10:16:34 +0300

    check_data

---
+++ scripts/check_data.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Users:'
+curl --ipv4 --location 'http://localhost:4567/api/users' | jq . | more
+echo 'Object sights:'
+curl --ipv4 --location 'http://localhost:4567/api/objectSights' | jq . | more
+echo 'Questions:'
+curl --ipv4 --location 'http://localhost:4567/api/questions' | jq . | more
+echo 'Answers:'
+curl --ipv4 --location 'http://localhost:4567/api/answers/' | jq . | more
