2ce309a028e886c66c87d21a3d4decbcca6b60a0
517ANT38 <a.tashchilin@bk.ru>
2023-12-18 07:19:13 +0400

    add check docker group

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
 echo "Build started"
 
-cd $(dirname $0)/.. || exit 1
+gd=docker
+gr=$(groups "$USER" | grep -o -w "\b$gd\b")
 
-DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t app/appmarks .
+if [ "$gd" = "$gr" ]; then
+    cd $(dirname $0)/.. || exit 1
+    DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t app/appmarks .
+else
+    echo "User $USER is not a member of the Docker group."
+    exit 1
+fi
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-cd $(dirname $0)/.. || exit 1
-docker-compose restart
+gd=docker
+gr=$(groups "$USER" | grep -o -w "\b$gd\b")
+
+if [ "$gd" = "$gr" ]; then
+    cd $(dirname $0)/.. || exit 1
+    docker-compose restart
+else
+    echo "User $USER is not a member of the Docker group."
+    exit 1
+fi
