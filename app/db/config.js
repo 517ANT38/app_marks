8a659ba6ca31a759a72399275c2385829f124b83
anton <a.tashchilin@bk.ru>
2024-02-27 11:38:33 +0400

    add change

--- app/db/config.js
+++ app/db/config.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 module.exports={
-        database:process.env.DB_DOCKER,
-        username:process.env.USERNAME_DB_DOCKER,
-        port:process.env.PORT_DB_DOCKER,
-        dialect:process.env.DIALECT_DOCKER,
-        password:process.env.PASSWORD_DB_DOCKER,
-        host:process.env.HOST_DOCKER,
+        database:process.env.DB_APP,
+        username:process.env.USERNAME_DB_APP,
+        port:process.env.PORT_DB_APP,
+        dialect:process.env.DIALECT_APP,
+        password:process.env.PASSWORD_DB_APP,
+        host:process.env.HOST_APP,
         logging: false,
         timezone: '+04:00'
 }
