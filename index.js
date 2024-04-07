1de745cb337d0dfe9c0b4ac6dbb14dd1b36bac01
anton <a.tashchilin@bk.ru>
2024-04-07 22:17:52 +0400

    add change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const db=require("./app/db");
+const db = require("./app/db");
 const app = require("./app/app").app
 const PORT = process.env.PORT;
 const server = app.listen(PORT,()=>{
@@ -7,11 +7,14 @@ const server = app.listen(PORT,()=>{
 
 function closeApp(signal){
    db.connection.close().then(x=>{
-      server.close(() => {
-         console.log(`Server stop, signal ${signal}`);
+      server.close((err) => {
+         if (err) 
+            console.error('error shutdown server: %s',err.message)         
+         else
+            console.log(`Server stop, signal ${signal}`);
       });
-   }).catch(x=>console.log(x))
+   }).catch(e=>console.error('error off connect: %s',e.message))
 }
 
-process.on('SIGTERM', (e) => closeApp(e));
-process.on('SIGINT', (e) => closeApp(e));
+process.on('SIGTERM', (s) => closeApp(s));
+process.on('SIGINT', (s) => closeApp(s));
